7bd1c96a1dea497480697818282e22b82651a7d3
PC <isusnjara7@gmail.com>
2022-01-22 22:20:16 +0100

    boja search hovera

--- jqIkone.js
+++ jqIkone.js
@@ -4,10 +4,10 @@ $(document).ready(function(){
     iconWidth = parseFloat($('#search-icon').css('width'), 10);
     voiceWidth = parseFloat($('#voice-button').css('width'), 10) + parseFloat($('#voice-button').css('margin-left'), 10);
     formWidth = contWidth - iconWidth - voiceWidth;
+    $('#search-form').css('width', formWidth+'px');
 
     $(".circle").click(function(){
       $(".pozadina").css('background-color', 'red');
-      $('#search-form').css('width', '300px');
       
       alert('Ukupno: '+ contWidth+' icon: '+iconWidth+' voice: '+voiceWidth+' forma: '+formWidth);
     });
