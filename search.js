e556bd2fe120d09f57a6a317ef540eea33a42ee4
PC <isusnjara7@gmail.com>
2022-01-23 00:09:51 +0100

    new

--- search.js
+++ search.js
@@ -4,14 +4,11 @@ function showResult(str) {
       document.getElementById("livesearch").style.border="0px";
       return;
     }
-    else{
-        document.getElementById("livesearch").innerHTML="nije prazno!";
-    }
+
     var xmlhttp=new XMLHttpRequest();
     xmlhttp.onreadystatechange=function() {
       if (this.readyState==4 && this.status==200) {
         document.getElementById("livesearch").innerHTML=this.responseText;
-        document.getElementById("livesearch").style.border="1px solid #A5ACB2";
       }
     }
     xmlhttp.open("GET","livesearch.php?q="+str,true);
