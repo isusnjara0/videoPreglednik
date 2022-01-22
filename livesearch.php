746eadb7187053972d218f93ae1220c5399099c5
PC <isusnjara7@gmail.com>
2022-01-23 00:34:14 +0100

    dsa

--- livesearch.php
+++ livesearch.php
@@ -4,10 +4,8 @@ $xmlDoc->load("links.xml");
 
 $x=$xmlDoc->getElementsByTagName('link');
 
-//get the q parameter from URL
 $q=$_GET["q"];
 
-//lookup all links from the xml file if length of q>0
 if (strlen($q)>0) {
   $hint="";
   $hint = '<ul>
@@ -16,14 +14,11 @@ if (strlen($q)>0) {
   <li>Milk</li></ul>';
 }
 
-// Set output to "no suggestion" if no hint was found
-// or to the correct values
 if ($hint=="") {
   $response="no suggestion";
 } else {
   $response=$hint;
 }
 
-//output the response
 echo $response;
 ?> 
--- search.js
+++ search.js
@@ -7,7 +7,7 @@ function showResult(str) {
 
     var xmlhttp=new XMLHttpRequest();
     xmlhttp.onreadystatechange=function() {
-      if (this.readyState==4 && this.status==200) {
+    if (this.readyState==4 && this.status==200) {
         document.getElementById("livesearch").innerHTML=this.responseText;
       }
     }
