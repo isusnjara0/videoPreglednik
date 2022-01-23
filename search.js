a18f06b88ffcefb020c17e3c6c50c4a180213282
PC <isusnjara7@gmail.com>
2022-01-23 01:32:43 +0100

    unos

--- search.js
+++ search.js
@@ -1,18 +1,20 @@
 $(document).ready(function(){
     $('#search').keypress(function(){
-        $.ajax({
-            type: 'POST',
-            url: 'livesearch.php',
-            data:{
-                name:$('#search').val(),
-            },
-            function(data, status){
-                alert("Data: " + data + "\nStatus: " + status);
-            },
-            success:function(data){
-                $('#livesearch').html(data);
-            }
+        if($('#search').val()!=''){
+            $.ajax({
+                type: 'POST',
+                url: 'livesearch.php',
+                data:{
+                    name:$('#search').val(),
+                },
+                function(data, status){
+                    alert("Data: " + data + "\nStatus: " + status);
+                },
+                success:function(data){
+                    $('#livesearch').html(data);
+                }
 
-        });
+            });
+        }
     });
 });
