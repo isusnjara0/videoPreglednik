6eee71f5139838c131797631018b91f6305b2da8
PC <isusnjara7@gmail.com>
2022-01-23 01:07:48 +0100

    ajax update

--- search.js
+++ search.js
@@ -6,6 +6,9 @@ $(document).ready(function(){
             data:{
                 name:$('#search').val(),
             },
+            function(data, status){
+                alert("Data: " + data + "\nStatus: " + status);
+            },
             success:function(data){
                 $('#livesearch').html(data);
             }
